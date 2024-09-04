8d3b379298c2cf3223ddbd6bbac718695634f276
Ereklebazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2024-09-04 17:09:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let xmovnebi = 'aeiouAEIOU';
+let question = 'erekle';
+
+if(question.length > 0 && xmovnebi.includes(question[0])){
+    console.log('სიტყვა იწყება ხმოვანით')
+}else{
+    console.log('სიტყვა არ იწყება ხმოვანით')
+}
